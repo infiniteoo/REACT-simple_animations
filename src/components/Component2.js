118b6a86f642467adc101b59c5c8390f1d0dcd25
Troy Dorman <troydorman@gmail.com>
2021-08-01 06:29:19 -0600

    created component 2 and integrated it into app.js

---
+++ src/components/Component2.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Spring } from "react-spring";
+
+export class Component2 extends Component {
+  render() {
+    return (
+      <Spring from={{ opacity: 0 }} to={{ opacity: 1 }}>
+        {(props) => (
+          <div style={props}>
+            <div style={c2Style}>
+              <h1>Component 2</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maiores tenetur, cupiditate repellendus rerum, magnam provident
+                ut facere labore minima corporis a eveniet consequatur itaque
+                ullam inventore aut, dolore laudantium error.
+              </p>
+            </div>
+          </div>
+        )}
+      </Spring>
+    );
+  }
+}
+const c2Style = {
+  background: "slateblue",
+  color: "white",
+  padding: "1.5rem",
+};
+
+export default Component2;
