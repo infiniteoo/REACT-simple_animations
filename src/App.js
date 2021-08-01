c0ec066848a0295f353b6b61cec460769ab20c0d
Troy Dorman <troydorman@gmail.com>
2021-07-31 20:13:49 -0600

    created our first component and integrated it into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Component1 from "./components/Component1";
 
 function App() {
   return (
     <div className="App">
-      
+      <Component1 />
     </div>
   );
 }
---
+++ src/components/Component1.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Component1() {
+  return (
+    <div>
+      <h1>Component 1</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+        tenetur, cupiditate repellendus rerum, magnam provident ut facere labore
+        minima corporis a eveniet consequatur itaque ullam inventore aut, dolore
+        laudantium error.
+      </p>
+    </div>
+  );
+}
