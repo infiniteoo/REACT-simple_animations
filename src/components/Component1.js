8fbd70f1a906c01e75fa8aa9b7af72b1ce5deab3
Troy Dorman <troydorman@gmail.com>
2021-08-01 06:26:26 -0600

    imported react-spring.  wrapped existing div in spring component. added props for top down animation

--- src/components/Component1.js
+++ src/components/Component1.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { Spring } from "react-spring";
 
 export default function Component1() {
   return (
-    <div style={c1Style}>
-      <h1>Component 1</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
-        tenetur, cupiditate repellendus rerum, magnam provident ut facere labore
-        minima corporis a eveniet consequatur itaque ullam inventore aut, dolore
-        laudantium error.
-      </p>
-    </div>
+    <Spring
+      from={{ opacity: 0, marginTop: -500 }}
+      to={{ opacity: 1, marginTop: 0 }}
+    >
+      {(props) => (
+        <div style={props}>
+          <div style={c1Style}>
+            <h1>Component 1</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maiores
+              tenetur, cupiditate repellendus rerum, magnam provident ut facere
+              labore minima corporis a eveniet consequatur itaque ullam
+              inventore aut, dolore laudantium error.
+            </p>
+          </div>
+        </div>
+      )}
+    </Spring>
   );
 }
 
